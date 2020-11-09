d2d394be3be578d098baed91946979af702eec0b
Jordan <jordanleelong5@gmail.com>
2020-11-09 15:54:38 -0800

    created log in button

---
+++ src/components/LoginButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const LoginButton = (props) => {
+    console.log(props.onClick)
+    return (
+        <div>
+            <label>Login</label>
+            <button onClick={props.onClick}>LOGIN!</button>
+        </div>
+    )
+}
+
+export default LoginButton
