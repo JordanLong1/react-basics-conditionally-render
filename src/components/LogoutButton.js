6042c8c23cd79cfce716a455c4437811f8d486f7
Jordan <jordanleelong5@gmail.com>
2020-11-09 15:55:29 -0800

    added logout button

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const LogoutButton = (props) => {
+    return (
+        <div>
+            <label>LogOut</label>
+            <button onClick={props.onClick}>LogOut</button>
+        </div>
+    )
+}
+
+export default LogoutButton
