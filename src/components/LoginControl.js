6abb11d38cc6a75802bbf7f48db69a86528fb07c
Jordan <jordanleelong5@gmail.com>
2020-11-09 15:55:01 -0800

    Login Control to conditionally render whats shown based on the state

---
+++ src/components/LoginControl.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import {Greeting} from './Greeting'
+class LoginControl extends React.Component {
+
+    state = {isLoggedIn: false}
+
+
+    handleToggle = () => {
+        this.setState({
+            isLoggedIn: !this.state.isLoggedIn
+        })
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+          button = <LogoutButton onClick={this.handleToggle} />;
+        } else {
+          button = <LoginButton onClick={this.handleToggle} />;
+        }
+
+        return (
+        <div>
+            <Greeting isLoggedIn={isLoggedIn} />
+            {button}
+        </div>
+
+        )
+    }
+}
+
+export default LoginControl
